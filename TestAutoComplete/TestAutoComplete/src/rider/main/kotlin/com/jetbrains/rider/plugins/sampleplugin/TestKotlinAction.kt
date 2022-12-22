4c41746d2c967bc937db553940070da994366a9c
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2022-12-22 14:56:54 +0100

    Added Action to trigger the autocomplete process

--- TestAutoComplete/TestAutoComplete/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/TestKotlinAction.kt
+++ TestAutoComplete/TestAutoComplete/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/TestKotlinAction.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.rider.plugins.sampleplugin
 
-import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProcess
+import com.intellij.codeInsight.completion.CompletionService
+import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -8,16 +11,16 @@ import com.intellij.openapi.ui.Messages
 
 class TestKotlinAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val completionProcess = CompletionService.getCompletionService().currentCompletion
-        //val psiElement = event.getData(CommonDataKeys.PSI_ELEMENT)
         val file = event.getData(CommonDataKeys.PSI_FILE)
         val editor = event.getData(CommonDataKeys.EDITOR)
         val offset = editor?.caretModel?.offset
+        val completionProcess: CompletionProcess? = CompletionService.getCompletionService().currentCompletion
+        //val psiElement = event.getData(CommonDataKeys.PSI_ELEMENT)
         val psiElement = offset?.let { off -> file?.findElementAt(off - 1) }
         if (psiElement != null && file != null && completionProcess != null ) {
             val completionParams = CompletionParameters(psiElement, file, CompletionType.BASIC, offset, 0, editor, completionProcess)
             val provider = KotlinLookupElementProvider(completionParams)
-            val numberOfElements = "Success! " + provider.elements[0] + " is first Element"
+            val numberOfElements = "Success! " + provider.elements.size + " Elements found"
             Messages.showErrorDialog(numberOfElements, "LookUp Element Found!")
         } else {
             var message = "Failed: "
---
+++ TestAutoComplete/TestAutoComplete/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/TriggerAutoComplete.kt
@@ -0,0 +1,15 @@
+package com.jetbrains.rider.plugins.sampleplugin
+
+import com.intellij.codeInsight.AutoPopupController
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+
+class TriggerAutoComplete : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val editor = event.getData(CommonDataKeys.EDITOR)
+        val project = event.project
+        val popupController = project?.let { AutoPopupController.getInstance(it) }
+        popupController?.scheduleAutoPopup(editor)
+    }
+}

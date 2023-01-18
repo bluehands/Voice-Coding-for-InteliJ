9cf0fdd2b8dbbb398fa3ec7b8e68457930ca1bed
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-01-18 10:45:18 +0100

    Initial Commit for Voice Coding Plugin

---
+++ Voice Coding Plugin/VoiceCodingPlugin/src/dotnet/ReSharperPlugin.VoiceCodingPlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.VoiceCodingPlugin.Tests
+{
+    [ZoneDefinition]
+    public class VoiceCodingPluginTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IVoiceCodingPluginZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<VoiceCodingPluginTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class VoiceCodingPluginTestsAssembly : ExtensionTestEnvironmentAssembly<VoiceCodingPluginTestEnvironmentZone> { }
+}

e55f57dbbfade3d9f789fd1ab2afb739ff707299
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-02-13 16:32:40 +0100

    added logging

--- Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/listeners/LookupManagerListener.kt
+++ Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/listeners/LookupManagerListener.kt
@@ -2,11 +2,13 @@ package com.jetbrains.rider.plugins.voiceCodingPlugin.listeners
 
 import com.intellij.codeInsight.lookup.Lookup
 import com.intellij.codeInsight.lookup.LookupManagerListener
+import com.jetbrains.rider.plugins.voiceCodingPlugin.Logger
 import com.jetbrains.rider.plugins.voiceCodingPlugin.SpeechHandler
 
 class LookupManagerListener: LookupManagerListener {
-    override fun activeLookupChanged(old: Lookup?, new: Lookup?) {
-        val listener = LookupListener()
-        if (SpeechHandler.transcriptionEventFlag) new?.addLookupListener(listener)
+        override fun activeLookupChanged(old: Lookup?, new: Lookup?) {
+            Logger.write("Lookup Event!")
+            val listener = LookupListener()
+            if (SpeechHandler.codingModeEvent) new?.addLookupListener(listener)
     }
 }

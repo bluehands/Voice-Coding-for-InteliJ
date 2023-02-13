c8738167b25dde2fc70ea8881b046531893afa81
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-02-13 16:32:18 +0100

    Minor changes: - added logging - only handle speech if any lookup elements found

--- Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/listeners/LookupListener.kt
+++ Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/listeners/LookupListener.kt
@@ -3,7 +3,7 @@ package com.jetbrains.rider.plugins.voiceCodingPlugin.listeners
 import com.intellij.codeInsight.lookup.Lookup
 import com.intellij.codeInsight.lookup.LookupEvent
 import com.intellij.codeInsight.lookup.LookupListener
-import com.intellij.openapi.ui.Messages
+import com.jetbrains.rider.plugins.voiceCodingPlugin.Logger
 import com.jetbrains.rider.plugins.voiceCodingPlugin.SpeechHandler
 
 class LookupListener: LookupListener {
@@ -12,12 +12,14 @@ class LookupListener: LookupListener {
 
     override fun lookupShown(event: LookupEvent) {
         _lookup = event.lookup
-        SpeechHandler.transcriptionEventFlag = false
     }
 
     override fun uiRefreshed() {
         val newCount = _lookup.items.size
         if (newCount != currentCount) currentCount = newCount
-        else SpeechHandler.insertDictation(_lookup.items)
+        else if (currentCount != 0) {
+            Logger.write("Loaded autocomplete options, $currentCount found")
+            SpeechHandler.insertDictation(_lookup.items, false)
+        }
     }
 }

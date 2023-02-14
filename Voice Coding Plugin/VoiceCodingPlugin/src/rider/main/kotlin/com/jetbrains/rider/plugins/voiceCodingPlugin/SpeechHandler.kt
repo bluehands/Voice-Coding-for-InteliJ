03b730adcf2ea50d09f836798074dec524ed3bc2
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-02-14 16:56:08 +0100

    Changed insertion of Code to using the autocomplete-Process, where possible.

--- Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/SpeechHandler.kt
+++ Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/SpeechHandler.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.rider.plugins.voiceCodingPlugin
 
 import com.intellij.codeInsight.AutoPopupController
+import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -33,37 +34,45 @@ object SpeechHandler {
                 if (context != null) {
                     val editor = context.getData(CommonDataKeys.EDITOR)
                     val popupController = context.project?.let { AutoPopupController.getInstance(it) }
+                    editor?.caretModel?.moveCaretRelatively(-1, 0, false, false, true)
+                    editor?.caretModel?.moveCaretRelatively(1, 0, false, false, true)
                     Logger.write("$popupController will start transcription.")
                     popupController?.scheduleAutoPopup(editor)
                 }
             }
         }
     }
-    fun insertDictation(autocompleteItems: List<LookupElement>? = null, verbatim: Boolean = true) {
+    fun insertDictation(autocompleteItems: List<LookupElement>? = null, verbatim: Boolean = true, insertionContext: InsertionContext? = null) {
         codingModeEvent = false
-        val speechRecognizer = generateRecognizer(autocompleteItems)
+        val speechRecognizer = generateRecognizer(autocompleteItems, verbatim)
         val recognitionTask = speechRecognizer.recognizeOnceAsync()
-        val recognitionResult = recognitionTask.get()
-        val transcribedText = if (verbatim) camelCaseContraction(recognitionResult.text)
-                                else generateCodeFromTranscription(recognitionResult.text, autocompleteItems)
-        Logger.write("Text recognized: $transcribedText")
-        DataManager.getInstance().dataContextFromFocusAsync.onSuccess {context: DataContext? ->
-            if (context != null) {
-                val editor = context.getData(CommonDataKeys.EDITOR)
-                editor?.insertString(transcribedText)
-                editor?.caretModel?.moveCaretRelatively(transcribedText.length, 0, false, false, true)
-                val popupController = context.project?.let { AutoPopupController.getInstance(it) }
-                popupController?.scheduleAutoPopup(editor)
+        val recognitionResult = recognitionTask.get().text
+        val transcribedText = if (verbatim)  camelCaseContraction(recognitionResult)
+                                else generateCodeFromTranscription(recognitionResult, autocompleteItems, insertionContext)
+        Logger.write("Text recognized: $transcribedText.")
+        if (transcribedText != "" && transcribedText != " ")  {
+            DataManager.getInstance().dataContextFromFocusAsync.onSuccess {context: DataContext? ->
+                if (context != null) {
+                    val editor = context.getData(CommonDataKeys.EDITOR)
+                    editor?.insertString(transcribedText)
+                    editor?.caretModel?.moveCaretRelatively(transcribedText.length, 0, false, false, true)
+                    val popupController = context.project?.let { AutoPopupController.getInstance(it) }
+                    popupController?.scheduleAutoPopup(editor)
+                }
             }
         }
         Logger.write("Finished handling speech-to-code input.")
         VoiceController.finishListening()
     }
-    private fun generateRecognizer(autocompleteItems: List<LookupElement>?): SpeechRecognizer {
+    private fun generateRecognizer(autocompleteItems: List<LookupElement>?, verbatim: Boolean): SpeechRecognizer {
         val speechRecognizer = SpeechRecognizer(_speechConfig, _audioConfig)
         val phraseListGrammar = PhraseListGrammar.fromRecognizer(speechRecognizer)
-        for (word in basicDeclarations) phraseListGrammar.addPhrase(word)
-        if (autocompleteItems != null) {
+        if (verbatim) {
+            for (word in basicDeclarations) {
+                phraseListGrammar.addPhrase(word)
+            }
+        }
+        else if (autocompleteItems != null) {
             for (element in autocompleteItems) {
                 phraseListGrammar.addPhrase(element.toString())
             }
@@ -86,9 +95,11 @@ object SpeechHandler {
         return camelCaseString
     }
 
-    private fun generateCodeFromTranscription (transcription: String, autocompleteItems: List<LookupElement>?): String {
+    private fun generateCodeFromTranscription (transcription: String, autocompleteItems: List<LookupElement>?, context: InsertionContext?): String {
         var code = ""
         var exactMatch = false
+        var subStringOccurrences = 0
+        var elementIndex = -1
         Logger.write("Formatting $transcription.")
         var transcriptionString = transcription.lowercase()
         transcriptionString = transcriptionString.replace(" ", "")
@@ -96,35 +107,42 @@ object SpeechHandler {
         transcriptionString = transcriptionString.replace("?", "")
         transcriptionString = transcriptionString.replace("!", "")
         Logger.write("Searching for $transcriptionString.")
-        for (word in basicDeclarations) {
-            if (word == transcriptionString) {
-                code = word
-                exactMatch = true
-                Logger.write("Exact match found. Insert $code.")
-            }
-        }
-        if (autocompleteItems != null && !exactMatch){
-            var subStringOccurrences = 0
-            for (element in autocompleteItems) {
+        if (autocompleteItems != null && context != null && transcriptionString != ""){
+            for (index in autocompleteItems.indices) {
+            //autocompleteItems.forEachIndexed { index, element ->
+                val element = autocompleteItems[index]
                 val elementString = element.toString().lowercase()
                 if (elementString == transcriptionString) {
-                    code = element.toString()
+                    element.handleInsert(context)
                     exactMatch = true
-                    Logger.write("Exact match found. Insert $code.")
+                    Logger.write("Exact match found. Insert $elementString.")
                     break
                 }
                 if (elementString.contains(transcriptionString)) {
                     subStringOccurrences++
                     code = element.toString()
+                    elementIndex = index
                 }
             }
-            Logger.write("$subStringOccurrences occurrences of $transcriptionString found.")
-            if (subStringOccurrences > 1 && !exactMatch) {
-                code = transcriptionString
+            if (!exactMatch) {
+                Logger.write("$subStringOccurrences occurrences of $transcriptionString found.")
+                if (subStringOccurrences > 1) {
+                    code = transcriptionString
+                }
+                else if (subStringOccurrences == 1) {
+                    Logger.write("Directly insert Item matching $transcriptionString.")
+                    autocompleteItems[elementIndex].handleInsert(context)
+                }
+                else {
+                    Logger.write("ToDo: Try to match with distance.")
+                }
             }
         }
-        else if (!exactMatch){
-            Logger.write("Error: CodingMode but no Lookup-Elements!")
+        else if (transcriptionString != "") {
+            Logger.write("Error: Empty transcription.")
+        }
+        else {
+            Logger.write("Error: Missing Elements or Context.")
         }
         return code
     }

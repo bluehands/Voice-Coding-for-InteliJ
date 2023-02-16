dca045ffc02e498225ec00771ecaf736edd38df2
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-02-16 16:25:34 +0100

    Implemented and tested substring-hamming-distance heuristic

--- Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/ElementDistance.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.rider.plugins.voiceCodingPlugin
-
-class ElementDistance (val elementString: String, val transcriptionString: String) {
-
-    private fun calculateStringHammingDistance (string1: String, string2: String): Int {
-        return -1
-    }
-}

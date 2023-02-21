3bc9325c50de20d58258c3a8519f357d819b410b
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-02-21 16:48:47 +0100

    Tested homophone generator implementation

--- TestAutoComplete/TestAutoComplete/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/TestDistanceAction.kt
+++ TestAutoComplete/TestAutoComplete/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/TestDistanceAction.kt
@@ -4,8 +4,12 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ui.Messages
+import java.io.File
+
 
 class TestDistanceAction: AnAction() {
+
+    private val homophoneChecker = Homophones("C:/Users/Public/Documents/VoiceCodingPlugin/Homophones.txt")
     override fun actionPerformed(event: AnActionEvent) {
         val file = event.getData(CommonDataKeys.PSI_FILE)
         val editor = event.getData(CommonDataKeys.EDITOR)
@@ -15,9 +19,33 @@ class TestDistanceAction: AnAction() {
         val word = "Bruttosozialprodukt"
         val nodeString = psiElement?.node?.text ?: ""
         val similarity = MostSimilarSubstring(word, nodeString)
-        Messages.showErrorDialog("The smallest hamming distance of $nodeString to any substring of $word is ${similarity.getBestHammingDistance()}." +
-                "The substring is at position ${similarity.getResultIndex()}, the substring is ${similarity.getBestSubstring()}",
+        val homophones = compilePossibleHomophones("List exceed all beer")
+        for (homophone in homophones) File("C:/Users/Public/Documents/VoiceCodingPlugin/Log.txt").appendText("$homophone \n")
+        Messages.showErrorDialog("The smallest hamming distance of $nodeString to any substring of $word is ${similarity.getBestHammingDistance()}. " +
+                "The maximum allowed distance is ${nodeString.length / 5}. " +
+                "The substring is at position ${similarity.getResultIndex()}, the substring is ${similarity.getBestSubstring()}.",
             "Hamming Distance")
+    }
+
+    private fun compilePossibleHomophones (wordSequence: String): List<String> {
+        var resultList = mutableListOf("")
+        var partialResult = mutableListOf<String>()
+        val wordsWithPermutations: MutableList<List<String>> = mutableListOf()
+        for (word in wordSequence.split(" ")) {
+            var possibleHomophones = homophoneChecker.getHomophone(word)
+            if (possibleHomophones == null) possibleHomophones = listOf(word)
+            wordsWithPermutations.add(possibleHomophones)
+        }
+        for (wordWithPermutations in wordsWithPermutations) {
+            for (result in resultList) {
+                for (permutation in wordWithPermutations) {
+                    partialResult.add(result + permutation)
+                }
+            }
+            resultList = partialResult
+            partialResult = mutableListOf()
+        }
+        return resultList
     }
 }
 
@@ -74,5 +102,31 @@ class MostSimilarSubstring (private val primaryString: String, private val compa
             if (longestString[i] == shortestString[i]) hammingDistance--
         }
         return hammingDistance
+    }
+}
+
+
+
+class Homophones (homophoneListFile: String) {
+    private val homophoneList: MutableList<List<String>> = mutableListOf()
+
+    init {
+        val homophoneSource = File(homophoneListFile).inputStream()
+        homophoneSource.bufferedReader().forEachLine {
+            val firstChar = it[0]
+            if (firstChar != '%') {
+                var homophone = it.split(",")
+                if (homophone.size > 1) homophoneList.add(homophone)
+            }
+        }
+    }
+
+    fun getHomophone (word: String): List<String>? {
+        for (homophones in homophoneList) {
+            for (homophone in homophones) {
+                if (word.lowercase() == homophone) return homophones
+            }
+        }
+        return null
     }
 }

193f23d6910056aa8855702377a18f88ad235de2
BLUEHANDS\philipp.bott <philipp.bott@bluehands.de>
2023-04-13 17:12:27 +0200

    Added sanity check to startup

--- Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/userActions/StartVoiceControls.kt
+++ Voice Coding Plugin/VoiceCodingPlugin/src/rider/main/kotlin/com/jetbrains/rider/plugins/voiceCodingPlugin/userActions/StartVoiceControls.kt
@@ -2,11 +2,18 @@ package com.jetbrains.rider.plugins.voiceCodingPlugin.userActions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+import com.jetbrains.rider.plugins.voiceCodingPlugin.UserParameters
 import com.jetbrains.rider.plugins.voiceCodingPlugin.VoiceController
 
 class StartVoiceControls: AnAction() {
     override fun actionPerformed(p0: AnActionEvent) {
-        VoiceController.startController()
+        if (UserParameters.azureSubscriptionKey == "" || UserParameters.azureRegionKey == "") {
+            Messages.showErrorDialog("Missing azure speech service.", "Error!")
+        }
+        else {
+            VoiceController.startController()
+        }
     }
 
 }
